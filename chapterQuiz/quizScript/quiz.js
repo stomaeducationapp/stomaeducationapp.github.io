86b8b0d70483002fa1b084f247671c0bf37d1c80
Jeremy Dunnet <17160124@student.curtin.edu.au>
2018-08-25 17:04:25 +0800

    - Added the new js, css and html file for chpater 1 quiz - no code yet

---
+++ chapterQuiz/quizScript/quiz.js
@@ -0,0 +1,3 @@
+// This is the javascript file for building, handling and displaying the chapter quizzes
+// Each quiz .html page will import identifiers to which chapter they are for - allowing the database to return chapter unique questions
+// The rest of the code is the same for each quiz - which is why we do not need more than one file
