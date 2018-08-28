94f535c878b159b5a2f12bd7a13ec689c9f0d339
caseyRogers <casey.rogers@student.curtin.edu.au>
2018-08-28 17:33:55 +0800

    Drop down changed

--- headers.js
+++ headers.js
@@ -1,20 +1,16 @@
 // JavaScript source code
-function headerFunction() {
-    document.getElementById("header1").classList.toggle("show");
-}
+var dropdown = document.getElementByClassName("dropdown-btn");
+var i;
 
-function filterFunction() {
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    div = document.getElementById("header1");
-    a = div.getElementByTagName("a");
-    for (i = 0; i < a.length; i++) {
-        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
-            a[i].style.display = "";
+for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
         }
         else {
-            a[i].style.display = "none";
+            dropdownContent.style.display = "block";
         }
-    }
+    });
 }
