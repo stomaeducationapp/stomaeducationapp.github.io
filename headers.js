299b4062f0e0de4ccec1f59bd3f408e3d6a58dd2
caseyRogers <casey.rogers@student.curtin.edu.au>
2018-08-29 10:11:24 +0800

    Content headers editted with small style changes Added javascript within file

--- headers.js
+++ headers.js
@@ -1,15 +1,14 @@
-// JavaScript source code
-var dropdown = document.getElementByClassName("dropdown-btn");
+//* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
 
 for (i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener("click", function() {
+    dropdown[i].addEventListener("click", function () {
         this.classList.toggle("active");
         var dropdownContent = this.nextElementSibling;
         if (dropdownContent.style.display === "block") {
             dropdownContent.style.display = "none";
-        }
-        else {
+        } else {
             dropdownContent.style.display = "block";
         }
     });
