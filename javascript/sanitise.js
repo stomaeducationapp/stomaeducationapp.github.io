ccb8bdac6e99734d79806a235860262c3124ca58
Jeremy Dunnet <17160124@student.curtin.edu.au>
2018-09-22 15:09:58 +0800

    - Semi-working search bar functionality

--- javascript/sanitise.js
+++ javascript/sanitise.js
@@ -1,13 +1,33 @@
-// JavaScript source code
+/* AUTHOR INFORMATION
+ * CREATOR - Ethan Bell 08/09/2018 (Originl Code), Jeremy Dunnet 22/09/2018 (This file)
+ * LAST MODIFIED BY - Jeremy Dunnet 22/09/2018 
+ */
 
+/* CLASS/FILE DESCRIPTION
+ * This javascript file handles the sanitisation of user input for a variety of actions a user on a website may have to provide input for.
+ * This protects the website from malicious and also helps user's who make mistakes to not fail in their inputs (if they accidentely typed a bad character)
+ */
 
-/* These functions allow sanitising input text and HTML web storage via the key. */
+/* VERSION HISTORY
+ * 22/09/2018 - Created file, adapted code and integrated into program
+ */
 
-/* This function sanitises user input via an imported string and returns the sanitised input */
+/* REFERENCES
+ * The original code was adapted from project team member Ethan Bell's feature design (/previousFiles/sanitisation/* to integrate into the current program as a whole
+ * And many tutorials/documentation from https://www.w3schools.com 
+ */
+
+/* FUNCTION INFORMATION
+ * NAME - sanitiseSearch
+ * INPUTS - inTxt (The original user input)
+ * OUTPUTS - outTxt (the sanitised input)
+ * PURPOSE - This function takes in a an input string (from any search bar text box) and removes all unwanted characters (anything that is not a number/A-Z)
+ *           and then returns it to the function that called it
+ */
 function sanitiseSearch(inTxt)
 {
     var outTxt, maxLen;
-    maxLen = 12;
+    maxLen = 26; //Tjis is based on the longest (CURRENTLY) available search tag + 3 for different word choice
 
     //regex to remove all non alphanumeric or whitespace characters
     outTxt = inTxt.replace(/[^0-9a-zA-Z ]/g, "");
@@ -18,21 +38,31 @@ function sanitiseSearch(inTxt)
         outTxt = outTxt.slice(0, maxLen);
     }
 
-    //
-    //Add new rules here if necessary in future
-    //
+    //Add new sanitise rules here if necessary in future
+
+    //CONSIDER
+    //This function can also do the searching of the document if thought more secure to not have the sanitised input move around before use
 
     return outTxt;
 }
 
-/* This function may be able to be combined with the function above, depending on the implementation of the web storage.
- * If the caller reads the text and passes it, the function above can be used. If the caller passes the key, this function will be used. */
+/* FUNCTION INFORMATION
+ * NAME - sanitiseWebStorage
+ * INPUTS - inKey (key used to get storage item)
+ * OUTPUTS - outRef (sanitised object)
+ * PURPOSE - This takes in a key refernce to get something out of web storage - we then chjeck what comes out and sanitise in case
+ *           an attacker managed to get malicious data into the storage and is trying to use it
+ */
 function sanitiseWebStorage(inKey)
 {
     var inRef, outRef, maxLen;
-    maxLen = 12;
+    maxLen = 12; //Set length to length of largest key +  a couple for spacing
 
-    inRef = localStorage.getItem(inKey);
+    //CONSIDER
+    //You may use the above sanitise function to sanitise the inKey value to ensure not malicious
+    //You may also want to have a switch/loop to check for illegal object values - prevent users from accessing privileged objects
+
+    inRef = localStorage.getItem(inKey); //Find the item associated with the key
 
     //regex to remove all non alphanumeric or whitespace characters
     outRef = inRef.replace(/[^0-9a-zA-Z ]/g, "");
@@ -43,9 +73,7 @@ function sanitiseWebStorage(inKey)
         outRef = outRef.slice(0, maxLen);
     }
 
-    //
-    //Add new rules here if necessary in future
-    //
+    //Add new sanitise rules here if necessary in future
 
     return outRef;
 }
--- javascript/search.js
+++ javascript/search.js
@@ -1,4 +1,28 @@
-// JavaScript source code
+﻿/* AUTHOR INFORMATION
+ * CREATOR - Oliver Yeudall 20/09/2018 (Original Code), Jeremy Dunnet 22/09/2018 (This file)
+ * LAST MODIFIED BY - Jeremy Dunnet 22/09/2018 
+ */
+
+/* CLASS/FILE DESCRIPTION
+ * This javascript file handles the functionality of searching the page (particularly the chapter dropdown side bar CURRENTLY) for
+ * for specific tags. This allows a user to find chapters with information they want more quickly. It also contains functionailty to 
+ * control how the search bar displays itself.
+ */
+
+/* VERSION HISTORY
+ * 22/09/2018 - Created file, adapted code and integrated into program
+ */
+
+/* REFERENCES
+ * All code was adapted from team member Oliver Yeudall's feature design (/previousFiles/searchbar.html) to integrate into the current program as a whole
+ * Detecting click events relative to search bar learned from http://blustemy.io/detecting-a-click-outside-an-element-in-javascript/
+ * Altering display based on key press learned from http://www.w3schools.com/howto/howto_js_filterdropdown.asp
+ * Dynamic array expanding learned from https://www.w3schools.com/jsref/jsref_push.asp
+ * Replace regex learned from https://stackoverflow.com/questions/16576983/replace-multiple-characters-in-one-replace-call
+ * Triggering click events from code learned from https://stackoverflow.com/questions/14156327/how-to-call-a-code-behinds-button-click-event-using-javascript
+ * Using data-* attritube learned from https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes
+ * And many tutorials/documentation from https://www.w3schools.com 
+ */
 
 
 /**************************************************************************
@@ -8,12 +32,10 @@
 *  IMPORT: none                                                           *
 *  EXPORT: none                                                           *
 *  ***********************************************************************/
-//Based on example from http://blustemy.io/detecting-a-click-outside-an-element-in-javascript/
-document.addEventListener("click", function (evt)
+/*document.addEventListener("click", function (evt)
 {
     var flyoutElement = document.getElementById("dropdownUI");
     var clickFlag = false;
-    //let targetElement = evt.target;
     var targetElement = evt.target;
 
     do
@@ -42,7 +64,7 @@ document.addEventListener("click", function (evt)
     clickFlag = false;
 
     return;
-});
+}); */
 
 
 /**************************************************************************
@@ -52,9 +74,25 @@ document.addEventListener("click", function (evt)
 *  EXPORT: none                                                           *
 *  ***********************************************************************/
 
-function searchFilter()
+function searchFilter(matches)
 {
-    document.querySelector("#dropDownList").style.display = "block";
+    results = document.getElementById("searchList");
+    results.innerHTML = matches.join("");
+
+    items = document.getElementsByClassName("searchItem");
+    for (ii = 0; ii < items.length; ii++)
+    {
+
+        items[ii].addEventListener('click', function ()
+        {
+            relatedID = this.dataset.targetId;
+            relatedAnchor = document.getElementById(relatedID);
+            relatedAnchor.click();
+        });
+
+    }
+
+    document.querySelector("#searchList").style.display = "block";
 }
 
 
@@ -80,43 +118,61 @@ function searchFilterBlur()
 
 function displayFilter()
 {
-    /* Based on example from www.w3schools.com/howto/howto_js_filterdropdown.asp */
 
     var rawUserInput, div, a;
-
     var searchValue = "";
+    var matches = []; //An empty array of found matches
 
-    rawUserInput = document.getElementById("inputBox");
+    //Get the box with the user's input
+    rawUserInput = document.getElementById("searchBar");
 
-    console.log(rawUserInput.value);
-
+    //Get input text, sanitise and convert to upper case - IN CURRENT IMPLEMENTATION CASE DOES NOT ALTER RESULTS
     searchValue = String(sanitiseSearch(rawUserInput.value)).trim().toUpperCase();
+    //CONSIDER
+    //Just obtaining the value if there is a difference between terms with case variance (proper names, acronyms etc.)
 
-    console.log(searchValue);
+    //Get the area the user is searching (each chapter tab has search terms applied based on if the subject is covered there)
+    //This is what the user is looking for
+    div = document.getElementById("sideNav");
 
-    div = document.getElementById("dropDownList");
-
+    //Since we use anchor tags to store the chapter tabs - we can strip out all uneeded HTML tags so our loop is as short as possible
     a = div.getElementsByTagName("a");
 
     var foundMatch = false;
 
-    for (var i = 0; i < a.length; i++)
+    //Loop through all found elements
+    for (var ii = 0; ii < a.length; ii++)
     {
-        if (String(a[i].innerHTML).trim().toUpperCase().indexOf(searchValue) > -1)
+        //If the tag contains the input in the search bar
+        if (String(a[ii].innerHTML).trim().toUpperCase().indexOf(searchValue) > -1)
         {
-            a[i].style.display = "";
+            //a[ii].style.display = ""; //Remove any existing display values (hidden/none) so that it is visible
+
             foundMatch = true;
+
+            //Assemble all the needed values from our found match
+            //ID
+            termID = a[ii].id;
+            //Innertext (minus any bookmarks so it looks clean)
+            resultText = (a[ii].innerText).replace("/…|✔|!|⋆/g", "");
+
+            //Create a div to contain our result - allows easier styling
+            //The div contains the acnhor we will use in searchFilter to load the chapter provided
+            result = '<div class="searchResult"> <a class="searchItem" data-target-id="' + termID + '">' + resultText + '</a>';
+
+            matches.push(result); //Add found tag to list of matches
         }
         else
         {
-            a[i].style.display = "none";
+            //a[ii].style.display = "none"; //Hide this element from search results
         }
 
     }
 
+    //A match was found - display all the items found, otherwise show an empty box
     if (foundMatch)
     {
-        searchFilter();
+        searchFilter(matches);
     } else
     {
         searchFilterBlur();
@@ -124,40 +180,8 @@ function displayFilter()
 
 }
 
+//Set up an event listener to trigger each time a user enters a single key (allows search to dynamically adjust to typed input)
 window.addEventListener('load', function ()
 {
-    document.getElementById('inputBox').addEventListener("keyup", displayFilter);
+    document.getElementById('searchBar').addEventListener("keyup", displayFilter);
 });
-
-
-/**************************************************************************
-*  Function: sanitiseSearch                                               *
-*  Purpose: Sanitize user input for non alphanumeric characters,          *
-*           whitespace characters and length                              *
-*  IMPORT: inTxt                                                          *
-*  EXPORT: outTxt                                                         *
-*  AUTHOR: Ethan Bell                                                     *
-*  DATE MODIFIED: 20/09/2018                                              *
-*  ***********************************************************************/
-
-/* This function sanitises user input via an imported string and returns the sanitised input */
-function sanitiseSearch(inTxt)
-{
-    var outTxt, maxLen;
-    maxLen = 12;
-
-    //regex to remove all non alphanumeric or whitespace characters
-    outTxt = inTxt.replace(/[^0-9a-zA-Z ]/g, "");
-
-    //only accept strings less than maxLen characters
-    if (outTxt.length > maxLen)
-    {
-        outTxt = outTxt.slice(0, maxLen);
-    }
-
-    //
-    //Add new rules here if necessary in future
-    //
-
-    return outTxt;
-}
